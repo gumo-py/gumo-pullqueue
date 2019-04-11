5c713176b4001efc25fb2883242b1bf63561ba12
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-04-11 15:17:39 +0900

    Add Configuration

---
+++ gumo/pullqueue/_configuration.py
@@ -0,0 +1,35 @@
+from logging import getLogger
+from typing import Optional
+
+from gumo.core.injector import injector
+from gumo.pullqueue.domain.configuration import PullQueueConfiguration
+
+logger = getLogger(__name__)
+
+
+class ConfigurationFactory:
+    @classmethod
+    def build(
+            cls,
+            default_queue_name: Optional[str] = None,
+    ) -> PullQueueConfiguration:
+        if default_queue_name is None:
+            return PullQueueConfiguration()
+
+        return PullQueueConfiguration(
+            default_queue_name=default_queue_name
+        )
+
+
+def configure(
+        default_queue_name: str,
+) -> PullQueueConfiguration:
+    config = ConfigurationFactory.build(
+        default_queue_name=default_queue_name,
+    )
+    logger.debug(f'Gumo.PullQueue is configured, config = {config}')
+
+    injector.binder.bind(PullQueueConfiguration, config)
+
+    return config
+
