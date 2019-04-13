cac8ec42c9899cc97551422c026c2c5f6ec53e30
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-04-13 20:06:51 +0900

    Expose gumo.pullqueue.enqueue

--- gumo/pullqueue/__init__.py
+++ gumo/pullqueue/__init__.py
@@ -2,11 +2,13 @@ from logging import getLogger
 
 from gumo.pullqueue._configuration import configure
 from gumo.pullqueue.domain.configuration import PullQueueConfiguration
+from gumo.pullqueue.application.enqueue import enqueue
 
 
 __all__ = [
     configure.__name__,
     PullQueueConfiguration.__name__,
+    enqueue.__name__,
 ]
 
 logger = getLogger('gumo.pullqueue')
