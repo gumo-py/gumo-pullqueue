bca59278e9f7fa3e2fc99197267fea306ced4830
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-04-17 22:26:15 +0900

    Expose gumo.pullqueue.PullTask

--- gumo/pullqueue/__init__.py
+++ gumo/pullqueue/__init__.py
@@ -1,10 +1,12 @@
 from gumo.pullqueue._configuration import configure
 from gumo.pullqueue.domain.configuration import PullQueueConfiguration
 from gumo.pullqueue.application.enqueue import enqueue
+from gumo.pullqueue.domain import PullTask
 
 
 __all__ = [
     configure.__name__,
     PullQueueConfiguration.__name__,
     enqueue.__name__,
+    PullTask.__name__,
 ]
