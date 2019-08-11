b0370aadc057447f9a52aa669851934a7dd2aee6
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-08-11 23:38:42 +0900

    Implements TaskEvent and add tests of domain models.

--- gumo/pullqueue/server/domain/__init__.py
+++ gumo/pullqueue/server/domain/__init__.py
@@ -2,6 +2,7 @@ import dataclasses
 import datetime
 import enum
 import copy
+import random
 
 from typing import Optional
 from typing import List
@@ -47,11 +48,54 @@ class PullTaskState:
     leased_by: Optional[PullTaskWorker] = None
 
     def _clone(self, **changes):
+        """
+        :rtype: PullTaskState
+        """
         return dataclasses.replace(self, **changes)
 
     def with_status(self, new_status: PullTaskStatus):
+        """
+        :rtype: PullTaskState
+        """
         return self._clone(
             status=new_status,
+        )
+
+    def with_lease_info(
+            self,
+            leased_at: Optional[datetime.datetime],
+            lease_expires_at: Optional[datetime.datetime],
+            leased_by: Optional[PullTaskWorker],
+    ):
+        """
+        :rtype: PullTaskState
+        """
+        return self._clone(
+            leased_at=leased_at,
+            lease_expires_at=lease_expires_at,
+            leased_by=leased_by,
+        )
+
+    def increment_count_if_needed(self):
+        """
+        :rtype: PullTaskState
+        """
+        return self._clone(
+            execution_count=self.execution_count + 1,
+            retry_count=self.retry_count + 1 if self.execution_count > 0 else 0
+        )
+
+    def with_schedules(
+            self,
+            last_executed_at: datetime.datetime,
+            next_executed_at: datetime.datetime
+    ):
+        """
+        :rtype: PullTaskState
+        """
+        return self._clone(
+            last_executed_at=last_executed_at,
+            next_executed_at=next_executed_at,
         )
 
 
@@ -84,41 +128,6 @@ class TaskEvent:
 
 
 @dataclasses.dataclass(frozen=True)
-class LeaseRequest(TaskEvent):
-    lease_time: int
-
-    def to_json(self) -> dict:
-        j = super(self).to_json()
-        j['lease_time'] = self.lease_time
-        return j
-
-
-@dataclasses.dataclass(frozen=True)
-class LeaseExtendRequest(TaskEvent):
-    lease_extend_time: int
-
-    def to_json(self) -> dict:
-        j = super(self).to_json()
-        j['lease_extend_time'] = self.lease_extend_time
-        return j
-
-
-@dataclasses.dataclass(frozen=True)
-class SuccessRequest(TaskEvent):
-    pass
-
-
-@dataclasses.dataclass(frozen=True)
-class FailureRequest(TaskEvent):
-    message: str
-
-    def to_json(self) -> dict:
-        j = super(self).to_json()
-        j['message'] = self.message
-        return j
-
-
-@dataclasses.dataclass(frozen=True)
 class GumoPullTask:
     """
     A class containing payload and metadata used internally in the Pull Queue.
@@ -134,9 +143,181 @@ class GumoPullTask:
         return self.task.key
 
     def _clone(self, **changes):
+        """
+        :rtype: GumoPullTask
+        """
         return dataclasses.replace(self, **changes)
 
     def with_status(self, new_status: PullTaskStatus):
+        """
+        :rtype: GumoPullTask
+        """
         return self._clone(
             state=self.state.with_status(new_status=new_status)
         )
+
+    def with_state(self, new_state: PullTaskState):
+        """
+        :rtype: GumoPullTask
+        """
+        return self._clone(
+            state=new_state
+        )
+
+    def add_event_log(self, event_log: TaskEvent):
+        """
+        :rtype: GumoPullTask
+        """
+        event_logs = copy.copy(self.event_logs)
+        event_logs.append(event_log)
+        return self._clone(
+            event_logs=event_logs
+        )
+
+
+@dataclasses.dataclass(frozen=True)
+class LeaseRequest(TaskEvent):
+    lease_time: int
+
+    def to_json(self) -> dict:
+        j = super(self).to_json()
+        j['lease_time'] = self.lease_time
+        return j
+
+    def build_next(self, task: GumoPullTask) -> GumoPullTask:
+        if task.state.status == PullTaskStatus.deleted:
+            raise ValueError(f'Target task can not change status, because already deleted.')
+
+        new_state = task.state.with_status(
+            new_status=PullTaskStatus.leased
+        ).with_lease_info(
+            leased_at=self.event_at,
+            lease_expires_at=self.event_at + datetime.timedelta(seconds=self.lease_time),
+            leased_by=self.worker,
+        ).increment_count_if_needed()
+
+        next_task = task.with_state(
+            new_state=new_state
+        ).add_event_log(
+            event_log=self
+        )
+
+        return next_task
+
+
+@dataclasses.dataclass(frozen=True)
+class LeaseExtendRequest(TaskEvent):
+    lease_extend_time: int
+
+    def to_json(self) -> dict:
+        j = super(self).to_json()
+        j['lease_extend_time'] = self.lease_extend_time
+        return j
+
+    def build_next(self, task: GumoPullTask) -> GumoPullTask:
+        if task.state.status == PullTaskStatus.deleted:
+            raise ValueError(f'Target task can not change status, because already deleted.')
+
+        if task.state.status != PullTaskStatus.leased:
+            raise ValueError(f'Target task status must equal as leased, but got: {task.state.status}')
+
+        new_state = task.state.with_lease_info(
+            leased_at=task.state.leased_at,
+            lease_expires_at=task.state.lease_expires_at + datetime.timedelta(seconds=self.lease_extend_time),
+            leased_by=task.state.leased_by,
+        )
+
+        next_task = task.with_state(
+            new_state=new_state
+        ).add_event_log(
+            event_log=self
+        )
+
+        return next_task
+
+
+@dataclasses.dataclass(frozen=True)
+class SuccessRequest(TaskEvent):
+    def build_next(self, task: GumoPullTask) -> GumoPullTask:
+        if task.state.status == PullTaskStatus.deleted:
+            raise ValueError(f'Target task can not change status, because already deleted.')
+
+        if task.state.status != PullTaskStatus.leased:
+            raise ValueError(f'Target task status must equal as leased, but got: {task.state.status}')
+
+        new_state = task.state.with_status(
+            new_status=PullTaskStatus.deleted
+        )
+
+        next_task = task.with_state(
+            new_state=new_state
+        ).add_event_log(
+            event_log=self
+        )
+
+        return next_task
+
+
+def exponential_back_off_with_jitters(
+        base_sleep_seconds: int,
+        attempts: int,
+        max_sleep_seconds: int,
+) -> int:
+    # The sleep time is an exponentially-increasing function of base_sleep_seconds.
+    # But, it never exceeds max_sleep_seconds.
+    sleep_seconds = min([base_sleep_seconds * (2 ** (attempts - 1)), max_sleep_seconds])
+    # Randomize to a random value in the range sleep_seconds/2 .. sleep_seconds
+    sleep_seconds = sleep_seconds * (0.5 * (1 + random.random()))
+    # But never sleep less than base_sleep_seconds
+    sleep_seconds = max(base_sleep_seconds, sleep_seconds)
+
+    return sleep_seconds
+
+
+@dataclasses.dataclass(frozen=True)
+class FailureRequest(TaskEvent):
+    message: str
+
+    def to_json(self) -> dict:
+        j = super(self).to_json()
+        j['message'] = self.message
+        return j
+
+    def build_next(
+            self,
+            task: GumoPullTask,
+            now: Optional[datetime.datetime] = None,
+    ) -> GumoPullTask:
+        if task.state.status == PullTaskStatus.deleted:
+            raise ValueError(f'Target task can not change status, because already deleted.')
+
+        if task.state.status != PullTaskStatus.leased:
+            raise ValueError(f'Target task status must equal as leased, but got: {task.state.status}')
+
+        if now is None:
+            now = datetime.datetime.utcnow()
+
+        sleep_seconds = exponential_back_off_with_jitters(
+            base_sleep_seconds=60,
+            attempts=task.state.execution_count,
+            max_sleep_seconds=3600 * 24,
+        )
+
+        new_state = task.state.with_status(
+            new_status=PullTaskStatus.available
+        ).with_lease_info(
+            leased_at=None,
+            lease_expires_at=None,
+            leased_by=None,
+        ).with_schedules(
+            last_executed_at=task.state.leased_at,
+            next_executed_at=now + datetime.timedelta(seconds=sleep_seconds)
+        )
+
+        next_task = task.with_state(
+            new_state=new_state
+        ).add_event_log(
+            event_log=self
+        )
+
+        return next_task
---
+++ tests/server/domain/test_gumo_pull_task.py
@@ -0,0 +1,290 @@
+import pytest
+import datetime
+
+from gumo.core import EntityKeyFactory
+
+from gumo.pullqueue.domain import PullTask
+from gumo.pullqueue.server.domain import PullTaskState
+from gumo.pullqueue.server.domain import PullTaskStatus
+from gumo.pullqueue.server.domain import PullTaskWorker
+from gumo.pullqueue.server.domain import GumoPullTask
+from gumo.pullqueue.server.domain import TaskEvent
+from gumo.pullqueue.server.domain import LeaseRequest
+from gumo.pullqueue.server.domain import LeaseExtendRequest
+from gumo.pullqueue.server.domain import SuccessRequest
+from gumo.pullqueue.server.domain import FailureRequest
+
+
+class TestPullTask:
+    def test_build_success(self):
+        task = PullTask(
+            key=EntityKeyFactory().build_for_new(kind='PullTask'),
+            queue_name='test'
+        )
+
+        assert isinstance(task, PullTask)
+        assert isinstance(task.to_json(), dict)
+        assert PullTask.from_json(task.to_json()) == task
+
+
+class TestPullTaskState:
+    def test_build_success(self):
+        state = PullTaskState(
+            status=PullTaskStatus.available,
+        )
+        assert state.status == PullTaskStatus.available
+        assert state.execution_count == 0
+        assert state.retry_count == 0
+        assert state.last_executed_at is None
+        assert state.next_executed_at is None
+        assert state.leased_at is None
+        assert state.lease_expires_at is None
+        assert state.leased_by is None
+
+    def test_with_status(self):
+        state = PullTaskState(status=PullTaskStatus.available)
+        assert state.with_status(
+            new_status=PullTaskStatus.leased
+        ).status == PullTaskStatus.leased
+
+    def test_with_lease_info(self):
+        now = datetime.datetime.utcnow()
+        state = PullTaskState(status=PullTaskStatus.available)
+        new_state = state.with_lease_info(
+            leased_at=now,
+            lease_expires_at=now + datetime.timedelta(seconds=300),
+            leased_by=PullTaskWorker(address='127.0.0.1', name='worker:1')
+        )
+
+        assert new_state != state
+
+    def test_increment_count_if_needed(self):
+        state = PullTaskState(status=PullTaskStatus.available)
+        new_state = state.increment_count_if_needed()
+
+        assert new_state != state
+        assert new_state.execution_count == 1
+        assert new_state.retry_count == 0
+
+        next_state = new_state.increment_count_if_needed()
+        assert next_state != new_state
+        assert next_state.execution_count == 2
+        assert next_state.retry_count == 1
+
+
+class TestGumoPullTask:
+    def build(self) -> GumoPullTask:
+        return GumoPullTask(
+            task=PullTask(
+                key=EntityKeyFactory().build_for_new(kind='PullTask'),
+                queue_name='test'
+            ),
+            state=PullTaskState(),
+            event_logs=[]
+        )
+
+    def test_build_success(self):
+        t = GumoPullTask(
+            task=PullTask(
+                key=EntityKeyFactory().build_for_new(kind='PullTask'),
+                queue_name='test'
+            ),
+            state=PullTaskState(),
+            event_logs=[]
+        )
+        assert isinstance(t, GumoPullTask)
+
+    def test_with_status(self):
+        task = self.build()
+        changed = task.with_status(new_status=PullTaskStatus.leased)
+
+        assert task != changed
+        assert task.state.status != changed.state.status
+
+    def test_add_event_log(self):
+        now = datetime.datetime.utcnow()
+        task = self.build()
+        changed = task.add_event_log(
+            event_log=TaskEvent(
+                event_at=now,
+                worker=PullTaskWorker(address='127.0.0.1', name='worker:1')
+            )
+        )
+
+        assert len(task.event_logs) == 0
+        assert len(changed.event_logs) == 1
+
+
+class TestLeaseRequest:
+    def build(self) -> GumoPullTask:
+        return GumoPullTask(
+            task=PullTask(
+                key=EntityKeyFactory().build_for_new(kind='PullTask'),
+                queue_name='test'
+            ),
+            state=PullTaskState(),
+            event_logs=[]
+        )
+
+    def test_build_success(self):
+        now = datetime.datetime.utcnow()
+        task = self.build()
+        event = LeaseRequest(
+            event_at=now,
+            worker=PullTaskWorker(address='127.0.0.1', name='worker:1'),
+            lease_time=300
+        )
+
+        next_task = event.build_next(task=task)
+
+        assert task != next_task
+        assert len(next_task.event_logs) == len(task.event_logs) + 1
+        assert next_task.state.status == PullTaskStatus.leased
+        assert next_task.state.leased_at == now
+        assert next_task.state.lease_expires_at == now + datetime.timedelta(seconds=300)
+
+
+class TestLeaseExtendRequest:
+    def build(self) -> GumoPullTask:
+        return GumoPullTask(
+            task=PullTask(
+                key=EntityKeyFactory().build_for_new(kind='PullTask'),
+                queue_name='test'
+            ),
+            state=PullTaskState(),
+            event_logs=[]
+        )
+
+    def build_leased_task(self) -> GumoPullTask:
+        now = datetime.datetime.utcnow()
+        task = self.build()
+        event = LeaseRequest(
+            event_at=now,
+            worker=PullTaskWorker(address='127.0.0.1', name='worker:1'),
+            lease_time=300
+        )
+
+        return event.build_next(task=task)
+
+    def test_build_success(self):
+        now = datetime.datetime.utcnow()
+        leased_task = self.build_leased_task()
+        event = LeaseExtendRequest(
+            event_at=now,
+            worker=PullTaskWorker(address='127.0.0.1', name='worker:1'),
+            lease_extend_time=300,
+        )
+
+        next_task = event.build_next(task=leased_task)
+
+        assert next_task != leased_task
+        assert len(next_task.event_logs) == len(leased_task.event_logs) + 1
+        assert next_task.state.status == leased_task.state.status
+        assert next_task.state.leased_at == leased_task.state.leased_at
+        assert next_task.state.lease_expires_at == leased_task.state.lease_expires_at + datetime.timedelta(seconds=300)
+        assert next_task.state.leased_by == leased_task.state.leased_by
+
+    def test_build_failed_from_available(self):
+        task = self.build()
+        now = datetime.datetime.utcnow()
+        event = LeaseExtendRequest(
+            event_at=now,
+            worker=PullTaskWorker(address='127.0.0.1', name='worker:1'),
+            lease_extend_time=300,
+        )
+
+        with pytest.raises(ValueError):
+            event.build_next(task=task)
+
+
+class TestSuccessRequest:
+    def build(self) -> GumoPullTask:
+        return GumoPullTask(
+            task=PullTask(
+                key=EntityKeyFactory().build_for_new(kind='PullTask'),
+                queue_name='test'
+            ),
+            state=PullTaskState(),
+            event_logs=[]
+        )
+
+    def build_leased_task(self) -> GumoPullTask:
+        now = datetime.datetime.utcnow()
+        task = self.build()
+        event = LeaseRequest(
+            event_at=now,
+            worker=PullTaskWorker(address='127.0.0.1', name='worker:1'),
+            lease_time=300
+        )
+
+        return event.build_next(task=task)
+
+    def test_build_success(self):
+        now = datetime.datetime.utcnow()
+        leased_task = self.build_leased_task()
+        event = SuccessRequest(
+            event_at=now,
+            worker=PullTaskWorker(address='127.0.0.1', name='worker:1'),
+        )
+
+        next_task = event.build_next(task=leased_task)
+
+        assert next_task != leased_task
+        assert len(next_task.event_logs) == len(leased_task.event_logs) + 1
+        assert next_task.state.status == PullTaskStatus.deleted
+
+    def test_build_failed_from_available(self):
+        task = self.build()
+        now = datetime.datetime.utcnow()
+        event = SuccessRequest(
+            event_at=now,
+            worker=PullTaskWorker(address='127.0.0.1', name='worker:1'),
+        )
+
+        with pytest.raises(ValueError):
+            event.build_next(task=task)
+
+
+class TestFailureRequest:
+    def build(self) -> GumoPullTask:
+        return GumoPullTask(
+            task=PullTask(
+                key=EntityKeyFactory().build_for_new(kind='PullTask'),
+                queue_name='test'
+            ),
+            state=PullTaskState(),
+            event_logs=[]
+        )
+
+    def build_leased_task(self) -> GumoPullTask:
+        now = datetime.datetime.utcnow()
+        task = self.build()
+        event = LeaseRequest(
+            event_at=now,
+            worker=PullTaskWorker(address='127.0.0.1', name='worker:1'),
+            lease_time=300
+        )
+
+        return event.build_next(task=task)
+
+    def test_build_success(self):
+        now = datetime.datetime.utcnow()
+        leased_task = self.build_leased_task()
+        event = FailureRequest(
+            event_at=now,
+            worker=PullTaskWorker(address='127.0.0.1', name='worker:1'),
+            message='something failed reason message.'
+        )
+
+        next_task = event.build_next(task=leased_task, now=now)
+
+        assert next_task != leased_task
+        assert next_task.state.status == PullTaskStatus.available
+        assert len(next_task.event_logs) == len(leased_task.event_logs) + 1
+
+        assert next_task.state.leased_at is None
+        assert next_task.state.lease_expires_at is None
+        assert next_task.state.leased_by is None
+
+        assert next_task.state.last_executed_at == leased_task.state.leased_at
+        assert next_task.state.next_executed_at >= now + datetime.timedelta(seconds=60)
